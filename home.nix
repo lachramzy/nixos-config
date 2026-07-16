441ba99226820f9341888361a85b3b8194714c90
lachlan <lacjamram@gmail.com>
2026-07-16 17:41:28 +1000

    auto: post-rebuild 2026-07-16 17:41:28

--- home.nix
+++ home.nix
@@ -119,6 +119,7 @@
   bind = $mainMod CTRL, O, exec, osu!
   bind = $mainMod, P, exec, prismlauncher
   bind = $mainMod, S, exec, steam
+  bind = $mainMod, PRINT, exec, hyprshot -m window
 
   bind = $mainMod, left, movefocus, l
   bind = $mainMod, right, movefocus, r
