cd07a06484bb85ee619844ea67638c14d96e66af
lachlan <lacjamram@gmail.com>
2026-09-07 15:31:50 +1000

    initial

---
+++ configuration.nix
@@ -0,0 +1,385 @@
+{ config, lib, pkgs, ... }:
+
+{
+############
+# > BOOT < #
+############
+
+  imports = [ ./hardware-configuration.nix ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+#  boot.initrd.luks.devices."enc" = {
+#    device = "/dev/disk/by-uuid/a7876e26-6371-4ed3-83bb-0deacbb82fc8";
+#    preLVM = true;
+#  };
+
+#  boot.initrd.luks.devices."cryptdata" = {
+#    device = "/dev/disk/by-uuid/a94af95f-5062-4c1a-aa2e-612645c19f38";
+#    preLVM = true;
+#  };
+
+#  fileSystems."/hdd" = {
+#    device = "/dev/disk/by-uuid/cb0134fc-6dd5-4efc-8114-b683b27b5e6f";
+#    fsType = "ext4";
+#    options = [ 
+#      "users" 
+#      "nofail"
+#    ];
+#  };
+
+  boot.blacklistedKernelModules = [
+    "ipv6"
+    "firewire-core"
+    "tb_net"
+    "floppy"
+    "cdrom"
+  ];
+
+  boot.kernelParams = [ "amd_pstate=active" ];
+  powerManagement.cpuFreqGovernor = "performance";
+  boot.tmp.useTmpfs = true;
+
+
+
+##############
+# > SYSTEM < #
+##############
+
+  networking.hostName = "nixos-btw";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" ];
+  networking.firewall = {
+    enable = true;
+  };
+  networking.networkmanager.enable = true;
+  networking.firewall.checkReversePath = false;
+
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.rmem_max" = 16777216;
+    "net.core.wmem_max" = 16777216;
+    "net.ipv4.tcp_rmem" = "4096 87380 16777216";
+    "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+  };
+
+  services.resolved.enable = true;
+  systemd.coredump.enable = false;
+  time.timeZone = "Australia/Melbourne";
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "monthly";
+  };
+  services.fstrim.enable = true;
+
+  security = {
+    apparmor.enable = true;
+    protectKernelImage = true;
+    sudo.extraConfig = ''
+      Defaults lecture="never"
+    '';
+    sudo.execWheelOnly = true;
+    forcePageTableIsolation = true;
+  };
+
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  hardware.i2c.enable = true;
+  boot.kernelModules = [ "i2c-dev" "uinput" ];
+
+  programs.fish = {
+  enable = true;
+  interactiveShellInit = ''
+    set -g fish_greeting
+
+    function nrs
+      set -l old_pwd $PWD
+        cd /etc/nixos
+
+        echo "=== Rebuilding system ==="
+
+        read --prompt-str "Update flake inputs? (y/N) " confirm
+        if test "$confirm" = y; or test "$confirm" = Y
+          echo "=== Updating flake inputs ==="
+          nix flake update
+        end
+
+        echo "=== Running nixos-rebuild ==="
+
+        if sudo nixos-rebuild switch --flake .#nixos-btw --cores 16 --max-jobs 2 $argv
+          echo "=== Build successful, committing changes ==="
+          git add .
+          if not git diff --cached --quiet
+            git commit -m "auto: post-rebuild "(date '+%Y-%m-%d %H:%M:%S')
+            git push
+            echo "✅ Pushed to GitHub"
+          else
+            echo "No changes to commit"
+          end
+          
+          cd $old_pwd
+          return 0
+        else
+          echo "❌ Rebuild failed, not committing"
+          
+          cd $old_pwd
+          return 1
+        end
+      end
+  '';
+    loginShellInit = ''
+      if test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
+          exec start-hyprland
+      end
+    '';
+  };
+
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      cores = 16;
+      max-jobs = 2;
+      http-connections = 128;
+      max-substitution-jobs = 128;
+    };
+  };
+
+  users.users.lachlan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "networkmanager" "i2c-dev" "plugdev" ];
+    packages = with pkgs; [
+      tree
+    ];
+  shell = pkgs.fish;
+  };
+
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", TAG+="uaccess"
+  '';
+
+  hardware.bluetooth.enable = false;
+  services.printing.enable = false;
+  networking.wireless.enable = lib.mkForce false;
+  documentation.enable = false;
+  documentation.man.enable = false;
+  documentation.doc.enable = false;
+  documentation.info.enable = false;
+
+  xdg.portal = {
+    enable = true;
+  };
+
+  environment.sessionVariables = {
+    AMD_VULKAN_ICD = "RADV";
+    RADV_PERFSET = "aco";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+
+
+
+#############
+# > SOUND < #
+#############
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    extraConfig = {
+      pipewire = {
+        "context.properties" = {
+          "default.clock.rate"          = 96000;
+          "default.clock.allowed-rates" = [ 44100 48000 88200 96000 ];
+          "default.clock.quantum"       = 1024;
+          "default.clock.min-quantum"   = 32;
+          "default.clock.max-quantum"   = 2048;
+        };
+      };
+    };
+  };
+
+
+
+################ 
+# > PACKAGES < #
+################
+
+  nixpkgs.config.allowUnfree = true;
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.iosevka
+      nerd-fonts.hack
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+    ];
+    enableDefaultPackages = true;
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" ];
+	sansSerif = [ "Noto Sans CJK JP" ];
+	serif = [ "Noto Serif CJK JP" ];
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    adwaita-icon-theme
+    alsa-plugins
+    alsa-utils
+    ani-cli
+    btop
+    cava
+    celluloid
+    cmatrix
+    croc
+    curl
+    davinci-resolve-studio
+    ddcutil
+    discord
+    easyeffects
+    ethtool
+    fastfetch
+    ffmpeg-full
+    gamescope
+    gammastep
+    gimp
+    git
+    git-crypt
+    gnome-keyring
+    gnome-themes-extra
+    hyprshot
+    kdePackages.filelight
+    kdePackages.qtwayland
+    keepassxc
+    kitty
+    lact
+    libreoffice
+    librewolf
+    libsecret
+    lutris
+    mpv
+    mov-cli
+    nixpkgs-fmt
+    nix-output-monitor
+    obsidian
+    obs-studio
+    openrgb
+    osu-lazer-bin
+    polychromatic
+    prismlauncher
+    protonup-qt
+    protontricks
+    proton-vpn-cli
+    pulsemixer
+    p7zip
+    ripgrep
+    spotify
+    swayimg
+    s-tui
+    termdown
+    thunar
+    tty-clock
+    unrar
+    unzip
+    vim
+    wget
+    winetricks
+    wineWow64Packages.stable
+    wireguard-tools
+    wl-clipboard
+    yazi
+    yt-dlp
+    zip
+  ];
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+  };
+
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+  programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
+
+
+
+###############
+# > DRIVERS < #
+###############
+
+  hardware.openrazer = {
+    enable = true;
+    users = [ "lachlan" ];
+  };
+  hardware.opentabletdriver.enable = true;
+  hardware.uinput.enable = true;
+
+
+
+###############
+# > AMD GPU < #
+###############
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+
+    package = pkgs.mesa.override {
+      stdenv = pkgs.stdenvAdapters.impureUseNativeOptimizations pkgs.stdenv;
+    };
+
+    package32 = pkgs.pkgsi686Linux.mesa.override {
+      stdenv = pkgs.pkgsi686Linux.stdenvAdapters.impureUseNativeOptimizations pkgs.pkgsi686Linux.stdenv;
+    };
+
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+    ];
+  };
+
+  environment.variables = {
+    RUSTICL_ENABLE = "radeonsi";
+  };
+
+  services.lact = {
+    enable = true;
+  };
+
+  hardware.amdgpu.overdrive.enable = true;
+
+
+
+####################
+# > NIX SETTINGS < #
+####################
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "26.05";
+}
---
+++ home.nix
@@ -0,0 +1,251 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "lachlan";
+  home.homeDirectory = "/home/lachlan";
+
+  programs.git.enable = true;
+  programs.git.settings.user.name = "lachlan";
+  programs.git.settings.user.email = "lacjamram@gmail.com";
+  
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin"
+  ];
+
+  # HYPRLAND RICE
+  home.file.".config/hypr/hyprland.conf" = {
+    text = ''
+  # MONITOR
+  monitor=DP-1,3840x2160@330,0x0,2,bitdepth,10,vrr,2
+
+  # ENVIRONMENT VARIABLES
+  env = XCURSOR_SIZE,32
+  env = HYPRCURSOR_SIZE,32
+
+  # LOOK AND FEEL
+  general {
+      gaps_in = 0
+      gaps_out = 0
+
+      border_size = 0
+
+      col.active_border = rgba(ffffffff) rgba(ffffffff) 45deg
+      col.inactive_border = rgba(ffffffff)
+
+      resize_on_border = false
+      allow_tearing = false
+
+      layout = dwindle
+  }
+
+  decoration {
+      rounding = 0
+      rounding_power = 0
+
+      active_opacity = 1
+      inactive_opacity = 1
+      fullscreen_opacity = 1
+
+      shadow {
+          enabled = false
+          range = 0
+          render_power = 0
+          color = rgba(00000000)
+      }
+
+      blur {
+          enabled = false
+          size = 0
+          passes = 0
+          new_optimizations = true
+          xray = false
+          vibrancy = 0
+      }
+  }
+
+  animations {
+      enabled = false
+  }
+
+  dwindle {
+      preserve_split = true
+  }
+
+  master {
+      new_status = master
+  }
+
+  # INPUT
+  input {
+      kb_layout = us
+      repeat_delay = 150
+      repeat_rate = 100
+
+      follow_mouse = 1
+      sensitivity = -0.8564
+      accel_profile = flat
+  }
+
+  misc {
+    disable_hyprland_logo = true
+    force_default_wallpaper = 0
+    background_color = rgb(000000)
+    disable_splash_rendering = true;
+  }
+
+  gesture = 3, horizontal, workspace
+
+  # KEYBINDS
+  $mainMod = SUPER
+
+  bind = $mainMod, RETURN, exec, kitty
+  bind = $mainMod, Q, killactive,
+  bind = $mainMod, M, exec, spotify
+  bind = $mainMod, E, exec, thunar
+  bind = $mainMod, V, togglefloating,
+  bind = $mainMod, P, pseudo,
+  bind = $mainMod, J, layoutmsg, togglesplit,
+  bind = $mainMod, W, exec, librewolf
+  bind = $mainMod, F, fullscreen, 0
+  bind = $mainMod, D, exec, discord
+  bind = $mainMod SHIFT, O, exec, libreoffice
+  bind = $mainMod, G, exec, gimp
+  bind = $mainMod SHIFT, D, exec, QT_QPA_PLATFORM=xcb davinci-resolve-studio
+  bind = $mainMod SHIFT, L, exec, lact
+  bind = $mainMod, L, exec, lutris
+  bind = $mainMod, A, exec, kitty --class floating_mixer -e pulsemixer
+  bind = $mainMod SHIFT, E, exec, easyeffects
+  bind = $mainMod, O, exec, obsidian
+  bind = $mainMod CTRL, O, exec, osu!
+  bind = $mainMod, P, exec, keepassxc
+  bind = $mainMod, S, exec, steam
+  bind = $mainMod SHIFT, P, exec, prismlauncher
+  bind = $mainMod, INSERT, exec, hyprshot -m window
+  bind = $mainMod, X, exec, hyprctl kill
+  bind = $mainMod SHIFT, F, exec, filelight
+
+  bind = $mainMod, left, movefocus, l
+  bind = $mainMod, right, movefocus, r
+  bind = $mainMod, up, movefocus, u
+  bind = $mainMod, down, movefocus, d
+
+  bind = $mainMod, 1, workspace, 1
+  bind = $mainMod, 2, workspace, 2
+  bind = $mainMod, 3, workspace, 3
+  bind = $mainMod, 4, workspace, 4
+  bind = $mainMod, 5, workspace, 5
+  bind = $mainMod, 6, workspace, 6
+  bind = $mainMod, 7, workspace, 7
+  bind = $mainMod, 8, workspace, 8
+  bind = $mainMod, 9, workspace, 9
+  bind = $mainMod, 0, workspace, 10
+
+  bind = $mainMod ALT, 1, movetoworkspace, 1
+  bind = $mainMod ALT, 2, movetoworkspace, 2
+  bind = $mainMod ALT, 3, movetoworkspace, 3
+  bind = $mainMod ALT, 4, movetoworkspace, 4
+  bind = $mainMod ALT, 5, movetoworkspace, 5
+  bind = $mainMod ALT, 6, movetoworkspace, 6
+  bind = $mainMod ALT, 7, movetoworkspace, 7
+  bind = $mainMod ALT, 8, movetoworkspace, 8
+  bind = $mainMod ALT, 9, movetoworkspace, 9
+  bind = $mainMod ALT, 0, movetoworkspace, 10
+
+  bind = $mainMod, mouse_down, workspace, e+1
+  bind = $mainMod, mouse_up, workspace, e-1
+
+  bindm = $mainMod, mouse:272, movewindow
+  bindm = $mainMod, mouse:273, resizewindow
+
+  bindel = $mainMod, Page_Up, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+
+  bindel = $mainMod, Page_Down, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-
+  bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+  bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+  bindel = $mainMod SHIFT, Page_Up, exec, ddcutil -b 10 setvcp 10 + 20
+  bindel = $mainMod SHIFT, Page_Down, exec, ddcutil -b 10 setvcp 10 - 20
+
+  bindl = , XF86AudioNext, exec, playerctl next
+  bindl = , XF86AudioPause, exec, playerctl play-pause
+  bindl = , XF86AudioPlay, exec, playerctl play-pause
+  bindl = , XF86AudioPrev, exec, playerctl previous
+  xwayland {
+      force_zero_scaling = true
+  }
+  exec-once = gnome-keyring-daemon --start --components=secrets
+  '';
+    force = true;
+  };
+
+  dconf.enable = true;
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
+      icon-theme = "Adwaita";
+      cursor-theme = "Adwaita";
+      cursor-size = 32;
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+      size = 32;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  qt = {
+    enable = true;
+    style.name = "adwaita-dark";
+    style.package = pkgs.adwaita-qt;
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 32;
+  };
+
+  home.sessionVariables = {
+    GTK_THEME = "Adwaita-dark";
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+  };
+
+  home.packages = with pkgs; [
+    gnome-themes-extra
+    adwaita-qt
+    adwaita-icon-theme
+    dconf
+    qt6Packages.qt6ct
+    papirus-icon-theme
+  ];
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      confirm_os_window_close = 0;
+      paste_actions = "no-op";
+    };
+  };
+
+  home.stateVersion = "26.05";
+}
