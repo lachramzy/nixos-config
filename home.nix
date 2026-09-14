c8035f88fb4912b8a4f56523e9d1e74f65677b19
lachlan <lacjamram@gmail.com>
2026-09-14 17:27:15 +1000

    auto: post-rebuild 2026-09-14 17:27:15

--- home.nix
+++ home.nix
@@ -22,7 +22,7 @@
         scale = 2,
         bitdepth = 10,
         vrr = 2,
-        cm = "srgb",
+        cm = "hdr",
         sdr_eotf = "srgb",
         supports_hdr = 1,
         supports_wide_color = 1,
