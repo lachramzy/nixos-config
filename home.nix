11103116235ec7d0100643954bb0226c4ef76111
lachlan <lacjamram@gmail.com>
2026-09-14 19:32:38 +1000

    auto: post-rebuild 2026-09-14 19:32:38

--- home.nix
+++ home.nix
@@ -25,7 +25,7 @@
         cm = "hdredid",
         sdr_eotf = "gamma22",
         supports_wide_color = 1,
-        supports_hdr = 1,,
+        supports_hdr = 1,
         sdr_min_luminance = 0.0,
         sdr_max_luminance = 700,
         min_luminance = 0.0,
