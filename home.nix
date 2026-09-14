3781eb2ecfa6b38a6dea90284ef0e6423dc0692e
lachlan <lacjamram@gmail.com>
2026-09-14 20:06:08 +1000

    auto: post-rebuild 2026-09-14 20:06:08

--- home.nix
+++ home.nix
@@ -27,7 +27,7 @@
         supports_wide_color = 1,
         supports_hdr = 1,
         sdrbrightness = 1.0,
-        sdrsaturation = 1.2,
+        sdrsaturation = 1.1,
         sdr_min_luminance = 0.0,
         sdr_max_luminance = 700,
         min_luminance = 0.0,
