1526e80b4528ab32f597b4545f3c33df1f669eed
lachlan <lacjamram@gmail.com>
2026-09-14 19:18:12 +1000

    auto: post-rebuild 2026-09-14 19:18:12

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@
         mode = "3840x2160@165",
         position = "0x0",
         scale = 2,
-        bitdepth = 32,
+        bitdepth = 8,
         vrr = 2,
         cm = "hdredid",
         sdr_eotf = "gamma22",
