c70bedb4fb7a5f3cfc7f7ca4eefdf328b8bbb07f
lachlan <lacjamram@gmail.com>
2026-09-17 18:04:53 +1000

    auto: post-rebuild 2026-09-17 18:04:53

--- home.nix
+++ home.nix
@@ -22,7 +22,7 @@
         scale = 2,
         bitdepth = 10,
         vrr = 2,
-        cm = "edid",
+        cm = "srgb",
         sdr_eotf = "gamma22force",
         supports_wide_color = 1,
         supports_hdr = 1,
